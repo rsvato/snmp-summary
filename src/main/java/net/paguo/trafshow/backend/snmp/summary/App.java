2aa22c0abdc8b429c9a2b5a40ef03fb0d35d5f28
slava <slava@11230f00-c60a-0410-812f-9e7afa6d8178>
2007-11-15 11:17:41 +0000

    Summary sceleton created
    git-svn-id: http://ns2.paguo.net/svn/repos/trafshow/trunk/backend/snmp-summary-app@509 11230f00-c60a-0410-812f-9e7afa6d8178

---
+++ src/main/java/net/paguo/trafshow/backend/snmp/summary/App.java
@@ -0,0 +1,13 @@
+package net.paguo.trafshow.backend.snmp.summary;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
