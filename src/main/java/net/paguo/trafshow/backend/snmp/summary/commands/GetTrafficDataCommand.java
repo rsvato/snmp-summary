08c332c237456e549e9e6227f396368aba31484a
slava <slava@11230f00-c60a-0410-812f-9e7afa6d8178>
2007-11-15 13:13:26 +0000

    Changes
    git-svn-id: http://ns2.paguo.net/svn/repos/trafshow/trunk/backend/snmp-summary-app@511 11230f00-c60a-0410-812f-9e7afa6d8178

--- src/main/java/net/paguo/trafshow/backend/snmp/summary/commands/GetTrafficDataCommand.java
+++ src/main/java/net/paguo/trafshow/backend/snmp/summary/commands/GetTrafficDataCommand.java
@@ -1,54 +1,65 @@
 package net.paguo.trafshow.backend.snmp.summary.commands;
 
-import java.util.Date;
+import net.paguo.trafshow.backend.snmp.summary.database.DBProxy;
+import net.paguo.trafshow.backend.snmp.summary.database.DBProxyFactory;
+import net.paguo.trafshow.backend.snmp.summary.model.TrafficRecord;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import net.paguo.trafshow.backend.snmp.summary.database.DBProxyFactory;
-import net.paguo.trafshow.backend.snmp.summary.database.DBProxy;
+
 import java.sql.Connection;
-import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class GetTrafficDataCommand implements DatabaseCommand<Object>{
-        private static final String SQL = "select dt, cisco, interface, inoctets, outoctets, uptime from tr_with_uptime t where dt between ? and ? order by cisco, interface, dt";
-        private static final Log log = LogFactory.getLog(GetTrafficDataCommand.class);
-        private Date start;
-        private Date end;
+public class GetTrafficDataCommand implements DatabaseCommand<Object> {
+    private static final String SQL = "select dt, cisco, interface, inoctets, outoctets, uptime from tr_with_uptime t where dt between ? and ? order by cisco, interface, dt";
+    private static final Log log = LogFactory.getLog(GetTrafficDataCommand.class);
+    private Date start;
+    private Date end;
 
-        public GetTrafficDataCommand(Date from, Date to){
-                this.start = from;
-                this.end = to;
-        }
+    public GetTrafficDataCommand(Date from, Date to) {
+        this.start = from;
+        this.end = to;
+    }
 
-        public Object getData(){
-                log.debug("getData(): <<<<");
-                DBProxy proxy = DBProxyFactory.getDBProxy();
-                Connection conn = null;
-                try{
-                        conn = proxy.getConnection();
-                        PreparedStatement pst = conn.prepareStatement(SQL);
-                        pst.setTimestamp(1, new Timestamp(start.getTime()));
-                        pst.setTimestamp(2, new Timestamp(end.getTime()));
-                        ResultSet rs = pst.executeQuery();
-                        while(rs.next()){
-                                ;
-                        }
-                        rs.close();
-                        pst.close();
-                }catch(SQLException e){
-                        log.error(e);
-                }finally{
-                        if (conn != null){
-                                try{
-                                        conn.close();
-                                }catch(SQLException e){
-                                        log.error("Error when closing connection", e);
-                                }
-                        }
+    public Object getData() {
+        log.debug("getData(): <<<<");
+        DBProxy proxy = DBProxyFactory.getDBProxy();
+        Connection conn = null;
+        List<TrafficRecord> result = new ArrayList<TrafficRecord>();
+        try {
+            conn = proxy.getConnection();
+            PreparedStatement pst = conn.prepareStatement(SQL);
+            pst.setTimestamp(1, new Timestamp(start.getTime()));
+            pst.setTimestamp(2, new Timestamp(end.getTime()));
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                TrafficRecord record = new TrafficRecord();
+                record.setDatetime(new Date(rs.getTimestamp(1).getTime()));
+                record.setRouter(rs.getString(2));
+                record.setIface(rs.getString(3));
+                record.setInput(rs.getLong(4));
+                record.setOutput(rs.getLong(5));
+                record.setUptime(rs.getLong(6));
+            }
+            rs.close();
+            pst.close();
+        } catch (SQLException e) {
+            log.error(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    log.error("Error when closing connection", e);
                 }
-                log.debug("getData(): >>>>");
-                return null;
+            }
         }
+        log.debug("getData(): >>>>");
+        return result;
+    }
 }
--- src/main/java/net/paguo/trafshow/backend/snmp/summary/model/TrafficRecord.java
+++ src/main/java/net/paguo/trafshow/backend/snmp/summary/model/TrafficRecord.java
@@ -1,11 +1,62 @@
 package net.paguo.trafshow.backend.snmp.summary.model;
 
+import java.util.Date;
 
-public class TrafficRecord{
-        private String router;
-        private String iface;
-        private Date datetime;
-        private Long input;
-        private Long output;
-        private Long uptime;
+
+public class TrafficRecord {
+    private String router;
+    private String iface;
+    private Date datetime;
+    private Long input;
+    private Long output;
+    private Long uptime;
+
+
+    public String getRouter() {
+        return router;
+    }
+
+    public void setRouter(String router) {
+        this.router = router;
+    }
+
+    public String getIface() {
+        return iface;
+    }
+
+    public void setIface(String iface) {
+        this.iface = iface;
+    }
+
+    public Date getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(Date datetime) {
+        this.datetime = datetime;
+    }
+
+    public Long getInput() {
+        return input;
+    }
+
+    public void setInput(Long input) {
+        this.input = input;
+    }
+
+    public Long getOutput() {
+        return output;
+    }
+
+    public void setOutput(Long output) {
+        this.output = output;
+    }
+
+    public Long getUptime() {
+        return uptime;
+    }
+
+    public void setUptime(Long uptime) {
+        this.uptime = uptime;
+    }
 }
