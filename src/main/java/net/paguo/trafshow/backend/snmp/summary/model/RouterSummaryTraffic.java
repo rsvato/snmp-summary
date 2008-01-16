a45e0b5091dab7883da932cdb472486e3d561841
slava <slava@11230f00-c60a-0410-812f-9e7afa6d8178>
2008-01-16 00:24:56 +0000

    Bug fixed
    git-svn-id: http://ns2.paguo.net/svn/repos/trafshow/trunk/backend/snmp-summary-app@606 11230f00-c60a-0410-812f-9e7afa6d8178

--- src/main/java/net/paguo/trafshow/backend/snmp/summary/model/RouterSummaryTraffic.java
+++ src/main/java/net/paguo/trafshow/backend/snmp/summary/model/RouterSummaryTraffic.java
@@ -33,8 +33,8 @@ public class RouterSummaryTraffic {
                 totalOutput += record.getOutput();
             }
         }else{
-            totalInput += record.getInput();
-            totalOutput += record.getOutput();
+            totalInput = 0L;
+            totalOutput = 0L;
         }
         this.lastProcessed = record;
     }
